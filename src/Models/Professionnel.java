f7874f2271d915155dd02c0876f41a634dbc695f
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-29 12:10:16 +0100

    here i add the Professionnel  class and getters and setters and construct

--- src/Models/Professionnel.java
+++ src/Models/Professionnel.java
@@ -1,4 +1,61 @@
 package Models;
 
-public class Professionnel {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Professionnel extends Person{
+
+    private Float revenu;
+    private String immatriculationFiscale;
+    private String secteurActivite;
+    private String activite;
+
+    public Professionnel(String nom, String prenom, String email, LocalDate dateNaissance, String ville, String adresse, String telephone, Integer nombreEnfants, Boolean investissement, Boolean placement, String situationFamiliale, Double score, Float revenu, String immatriculationFiscale, String secteurActivite, String activite) {
+        super(nom, prenom, email, dateNaissance, ville, adresse, telephone, nombreEnfants, investissement, placement, situationFamiliale, score);
+        this.revenu = revenu;
+        this.immatriculationFiscale = immatriculationFiscale;
+        this.secteurActivite = secteurActivite;
+        this.activite = activite;
+    }
+
+    public Professionnel(Integer id, String nom, String prenom, String email, LocalDate dateNaissance, String ville, String adresse, String telephone, Integer nombreEnfants, Boolean investissement, Boolean placement, String situationFamiliale, Double score, LocalDateTime dateCreation, Float revenu, String immatriculationFiscale, String secteurActivite, String activite) {
+        super(id, nom, prenom, email, dateNaissance, ville, adresse, telephone, nombreEnfants, investissement, placement, situationFamiliale, score, dateCreation);
+        this.revenu = revenu;
+        this.immatriculationFiscale = immatriculationFiscale;
+        this.secteurActivite = secteurActivite;
+        this.activite = activite;
+    }
+
+
+    public Float getRevenu() {
+        return revenu;
+    }
+
+    public void setRevenu(Float revenu) {
+        this.revenu = revenu;
+    }
+
+    public String getImmatriculationFiscale() {
+        return immatriculationFiscale;
+    }
+
+    public void setImmatriculationFiscale(String immatriculationFiscale) {
+        this.immatriculationFiscale = immatriculationFiscale;
+    }
+
+    public String getSecteurActivite() {
+        return secteurActivite;
+    }
+
+    public void setSecteurActivite(String secteurActivite) {
+        this.secteurActivite = secteurActivite;
+    }
+
+    public String getActivite() {
+        return activite;
+    }
+
+    public void setActivite(String activite) {
+        this.activite = activite;
+    }
 }
